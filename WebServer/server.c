5366307d4fa0d50469fd8dc4e5ea169feb549e87
CoderPro-young <1419006873@qq.com>
2021-01-25 19:20:28 +0800

    webserver

--- WebServer/server.c
+++ WebServer/server.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+
 
 #define MSGSIZE 1460 
-#define MAXLINE 1024  
+#define MAXLINE 1024
+#define LISTENQ 1024 
+#define SERVER_STRING "Server: Xuxy-TinyHttpd/0.1\r\n"
+#define HTTPPORT 2021 
 
 int read_line(int fd, char buf[], int MAXSIZE){
 	int i = 0;
 	char c;  
-	int n = 0; 
+	int n = 0;
+	//printf("fd is %d \n", fd);  
 	while(i < MAXSIZE){
 		if(recv(fd, &c, 1, 0) < 0){
 			perror("recv"); 
 			return -1; 
 		}
-		if(c == '\r'){
-			if(recv(fd, &c, 1, 0) < 0){
-					
-			}	
-			if(c == '\n'){
-				buf[i++] = '\0'; 
-				n++; 
-				break; 
-			}
+		printf("get %c \n", c); 
+		/*	
+		if(recv(fd, &c, 1, 0) < 0){
+			perror("recv")	; 
+			return -1; 	
+		}
+		*/	
+		if(c == '\n'){
+			buf[i++] = '\0'; 
+			n++; 
+			break; 
 		}
 		buf[i++] = c; 		
 		n++; 
@@ -35,14 +46,52 @@ int read_line(int fd, char buf[], int MAXSIZE){
 
 }
 
-int parse_url(){
-
-
+int parse_url(const char url[], char filename[]){
+	/*
+		parse right filename: return 1
+		parse wrong         : return 0 
+	*/
+	struct stat sbuf; 
+	strcpy(filename, "."); 
+	strcat(filename, url); 
+	/* filename is a dir name */
+	/*
+		stat(); 
+		if(...)
+	*/
+	if(url[strlen(url) - 1] == '/'){
+		strcat(filename, "index.html"); 
+	}
+	if(stat(filename, &sbuf) < 0){
+		return 0; 
+	}	
+	if((sbuf.st_mode & S_IFMT) == S_IFDIR){
+		strcat(filename, "/index.html"); 
+	}
+	return 1; 
 }
 
-void not_found(int fd){
-	const char errmsg[] = "404 NOT FOUND"; 
-	send(fd, errmsg, sizeof(errmsg), 0); 
+void not_found(int client){
+    char buf[1024];
+
+    sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, SERVER_STRING);
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<HTML><TITLE>Not Found</TITLE>\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<BODY><P>The server could not fulfill\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "your request because the resource specified\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "is unavailable or nonexistent.\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "</BODY></HTML>\r\n");
+    send(client, buf, strlen(buf), 0);
 }
 
 void cannot_read(int fd, char filename[]){
@@ -83,47 +132,76 @@ void serve_static(int fd, char filename[]){
 	body(fd, fp); 
 }
 
-void deal_request(void* arg){
-	int connfd = (int)arg; 
+void read_requesthead(int connfd){
+	char buf[MAXLINE]; 
+	while((read_line(connfd, buf, MAXLINE)) > 0 && strcmp(buf, "\r\n")){
+		printf("buf is %s \n"); 	
+	}
+	return ; 
+}
+
+void* deal_request(void* arg){
+	int connfd = *((int*)arg);
+	printf("connfd is %d \n", connfd);  
 	int is_static = 0; 
 	char buf[MAXLINE], method[MAXLINE], url[MAXLINE], version[MAXLINE]; 
 	char filename[MAXLINE]; 
 	struct stat sbuf; 
 
 	read_line(connfd, buf, MAXLINE); 
+	printf("buf is %s \n", buf); 
 	sscanf(buf, "%s %s %s", method, url, version); 
-
+	printf("url is %s \n", url); 
 	if(strcmp(method, "GET") == 0){
-		is_static = parse_url(url, filename); 
+		is_static = parse_url(url, filename);
+		printf("filename is %s \n", filename);  
 		if(is_static){
 			if(stat(filename, &sbuf) < 0){
-				not_found(connfd); 
-				return ; 
+				not_found(connfd);
+				read_requesthead(connfd);  
+				return (void*)(0); 
 			}
-			if(!(S_ISREG()) || !(S_IRUSR & sbuf.st_mode)){
-				cannot_read(connfd); 
+			if((sbuf.st_mode & S_IFMT) != S_IFREG || !(S_IRUSR & sbuf.st_mode)){
+				cannot_read(connfd, filename); 
 			}
 		}
-		serve_static(connfd, filename); ; 
+		else{
+			read_requesthead(connfd); 
+		}
+		serve_static(connfd, filename); 
 	}
-
-	return ; 
+	close(connfd); 
+	
+	return (void*)(0); 
 }
 
 int init_serverSocket(){
-	int servefd; 
+	int servefd;
+	int on = 1;  
 	servefd = socket(AF_INET, SOCK_STREAM, 0); 
 	
 	struct sockaddr_in sockaddr; 
 	socklen_t socklen = sizeof(sockaddr); 
 	bzero(&sockaddr, sizeof(sockaddr)); 
 
-	sockaddr.sa_family = AF_INET; 
+	sockaddr.sin_family = AF_INET; 
 	sockaddr.sin_port = htons(HTTPPORT); 
 	inet_pton(AF_INET, "0.0.0.0", &sockaddr.sin_addr); 
-
-	bind(servefd, (struct sockaddr*)sockaddr, socklen); 
-
+	
+	if((setsockopt(servefd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) < 0){
+		perror("setsockopt"); 
+		exit(1); 
+	}
+	if((bind(servefd, (struct sockaddr*)&sockaddr, socklen)) < 0){
+		perror("bind"); 
+		exit(1); 
+	}
+	
+	if(listen(servefd, LISTENQ) < 0){
+		perror("listen"); 
+		exit(1); 
+	}
+	
 	return servefd; 
 }
 
@@ -133,20 +211,22 @@ int main()
 	int serverSocket, connectionSocket; 
 	/* prepare a server socket */ 
 	serverSocket = init_serverSocket(); 
-	
+	struct sockaddr_in connaddr;
+	socklen_t connaddrlen = sizeof(connaddr);  
 	while(1){
 		// establish the connection
-		printf("ready to serve"); 
-		connectionSocket = accept();
+		printf("ready to serve\n"); 
+		bzero(&connaddr, sizeof(connaddr)); 
+		connectionSocket = accept(serverSocket, (struct sockaddr*)&connaddr, &connaddrlen);
 		if(connectionSocket < 0){
 			perror("accept"); 
 			exit(1); 
 		}
-		tid_t tid; 
-		if(pthread_create(&tid, NULL, &deal_request, (void*)connectionSocket) < 0){
+		pthread_t tid; 
+		if(pthread_create(&tid, NULL, deal_request, (void*)&connectionSocket) < 0){
 			perror("pthread_create"); 
 			exit(1); 
 		}
-		close(connectionSocket); 
+	//	close(connectionSocket); 
 	}
 }
